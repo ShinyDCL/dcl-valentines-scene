e740f74f4df7407ccf91418c19b84803124536eb
Ingaaa <106094638+ShinyDCL@users.noreply.github.com>
2023-09-26 20:30:46 +0300

    feat: add skybox

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const PARCEL_COUNT = 2 // 2 rows, 2 columns
+export const PARCEL_COUNT = 4 // 4 rows, 4 columns
 export const PARCEL_SIZE = 16
 export const SCENE_SIZE = PARCEL_COUNT * PARCEL_SIZE
 export const SCENE_MIDDLE = SCENE_SIZE / 2
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import { Vector3 } from '@dcl/sdk/math'
 import { setupLinks } from './links'
 import { SCENE_MIDDLE } from './config'
 import { setUpVideoPlayer } from './videoPlayer'
+import { setUpSkyBox } from './skyBox'
 
 export function main() {
   const scene = engine.addEntity()
   Transform.create(scene, { position: Vector3.create(SCENE_MIDDLE, 0, SCENE_MIDDLE) })
-  GltfContainer.create(scene, { src: 'models/templeLight.glb' })
+  GltfContainer.create(scene, { src: 'models/templeDark.glb' })
+
+  const border = engine.addEntity()
+  Transform.create(border, { parent: scene })
+  GltfContainer.create(border, { src: 'models/borderDark.glb' })
 
   setupLinks(scene)
   setUpVideoPlayer(scene)
+  setUpSkyBox(scene, 'images/skybox-dark')
 }
